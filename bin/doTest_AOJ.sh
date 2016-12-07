c99e6ef3a3e9f635f7b29a3a6568ee1e0d7a38a3
laikuaut <laikuaut@gmail.com>
2016-12-07 23:31:09 +0900

    一時コミット

--- bin/doTest_AOJ.sh
+++ bin/doTest_AOJ.sh
@@ -7,43 +7,44 @@ source ${SCRIPT_DIR}/modules.sh
 
 DIR_NAME=${1}
 Q_NAME=$(basename $DIR_NAME)
-DATA_DIR=test_data
+TEST_DATA_DIR=test_data
+RESULT_DATA_DIR=result_data
 
 # テストデータ取得
-mkdir ${DATA_DIR} -p
+mkdir ${TEST_DATA_DIR} -p
 for num in {1..100}
 do
     pre_num=$((num - 1))
-    if [ ! -e ${DATA_DIR}/${Q_NAME}_${num}_in.txt ];then
-        curl "http://analytic.u-aizu.ac.jp:8080/aoj/testcase.jsp?id=${Q_NAME}&case=${num}&type=in" -o ${DATA_DIR}/${Q_NAME}_${num}_in.txt >& /dev/null
-        grep "<html>" ${DATA_DIR}/${Q_NAME}_${num}_in.txt >& /dev/null
+    if [ ! -e ${TEST_DATA_DIR}/${Q_NAME}_${num}_in.txt ];then
+        curl "http://analytic.u-aizu.ac.jp:8080/aoj/testcase.jsp?id=${Q_NAME}&case=${num}&type=in" -o ${TEST_DATA_DIR}/${Q_NAME}_${num}_in.txt >& /dev/null
+        grep "<html>" ${TEST_DATA_DIR}/${Q_NAME}_${num}_in.txt >& /dev/null
         if [[ $? == 0 ]];then
-            rm ${DATA_DIR}/${Q_NAME}_${num}_in.txt
+            rm ${TEST_DATA_DIR}/${Q_NAME}_${num}_in.txt
             break
         fi
     fi
 
-    if [ ! -e ${DATA_DIR}/${Q_NAME}_${num}_out.txt ];then
-        curl "http://analytic.u-aizu.ac.jp:8080/aoj/testcase.jsp?id=${Q_NAME}&case=${num}&type=out" -o ${DATA_DIR}/${Q_NAME}_${num}_out.txt >& /dev/null
-        grep "<html>" ${DATA_DIR}/${Q_NAME}_${num}_in.txt >& /dev/null
+    if [ ! -e ${TEST_DATA_DIR}/${Q_NAME}_${num}_out.txt ];then
+        curl "http://analytic.u-aizu.ac.jp:8080/aoj/testcase.jsp?id=${Q_NAME}&case=${num}&type=out" -o ${TEST_DATA_DIR}/${Q_NAME}_${num}_out.txt >& /dev/null
+        grep "<html>" ${TEST_DATA_DIR}/${Q_NAME}_${num}_in.txt >& /dev/null
         if [[ $? == 0 ]];then
-            rm ${DATA_DIR}/${Q_NAME}_${num}_out.txt
+            rm ${TEST_DATA_DIR}/${Q_NAME}_${num}_out.txt
             break
         fi
     fi
 
     if [[ ${pre_num} != 0 ]];then
-        result=$(diff ${DATA_DIR}/${Q_NAME}_${num}_in.txt ${DATA_DIR}/${Q_NAME}_${pre_num}_in.txt)
+        result=$(diff ${TEST_DATA_DIR}/${Q_NAME}_${num}_in.txt ${TEST_DATA_DIR}/${Q_NAME}_${pre_num}_in.txt)
         if [ "${result}" = "" ];then
-            grep "<html>" ${DATA_DIR}/${Q_NAME}_${num}_in.txt >& /dev/null
-            grep "<html>" ${DATA_DIR}/${Q_NAME}_${num}_out.txt >& /dev/null
+            grep "<html>" ${TEST_DATA_DIR}/${Q_NAME}_${num}_in.txt >& /dev/null
+            grep "<html>" ${TEST_DATA_DIR}/${Q_NAME}_${num}_out.txt >& /dev/null
             break
         fi
     fi
-done
 
-# プログラム実行
-${SCRIPT_DIR}/run_AOJ.sh ${DIR_NAME}
+    # プログラム実行
+    ${SCRIPT_DIR}/run_AOJ.sh -i ${TEST_DATA_DIR}/${Q_NAME}_${num}_in.txt -o ${RESULT_DATA_DIR}/${Q_NAME}_${num}_out ${DIR_NAME}
+done
 
 # テストデータ削除
 #rm data/*
